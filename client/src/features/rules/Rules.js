8d6ceb485a577f46b229436fca4a72c2bd4f91c9
Cyrheel_ <cyrilb38@hotmail.com>
2022-07-03 23:21:40 +0200

    redux init soon, wip

--- client/src/features/rules/Rules.js
+++ client/src/features/rules/Rules.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import {
   handleName,
@@ -11,12 +11,23 @@ const Rules = () => {
   const gameName = useSelector(selectName);
   const nbOfPlayer = useSelector(selectNbOfPlayers);
   const dispatch = useDispatch();
-  const [name, setName] = useState("");
 
   return (
     <div>
       <label htmlFor="name">Game Name :</label>
-      <input type="text" id="name" />
+      <input
+        type="text"
+        id="name"
+        onChange={() => dispatch(handleName)}
+        value={gameName}
+      />
+      <lable htmlFor="nop">Number Of Players</lable>
+      <input
+        type="number"
+        id="nop"
+        onChange={() => dispatch(handleNbOfPlayers)}
+        value={nbOfPlayer}
+      />
     </div>
   );
 };
