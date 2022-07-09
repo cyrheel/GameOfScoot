373ff650fb578fcc3f60194b97680fe36ba231d6
Cyrheel_ <cyrilb38@hotmail.com>
2022-07-09 03:13:01 +0200

    first form done with redux data management

--- client/src/features/rules/Rules.js
+++ client/src/features/rules/Rules.js
@@ -1,34 +1,99 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import {
   handleName,
   handleNbOfPlayers,
-  selectName,
-  selectNbOfPlayers,
+  handleIsHard,
+  handleAllowRedo,
+  handleNbOfTry,
+  handleTargetWord,
+  handleLetters,
+  selectRules,
 } from "./rulesSlice";
 
 const Rules = () => {
-  const gameName = useSelector(selectName);
-  const nbOfPlayer = useSelector(selectNbOfPlayers);
   const dispatch = useDispatch();
+  const rules = useSelector(selectRules);
+  const [gameNameValue, setGameNameValue] = React.useState("");
+  const [nbOfPlayers, setNbOfPlayers] = React.useState(2);
+  const [isHard, setIsHard] = React.useState(false);
+  const [allowRedo, setAllowRedo] = React.useState(false);
+  // eslint-disable-next-line no-unused-vars
+  const [nbOfTry, setNbOfTry] = React.useState(1);
+  // eslint-disable-next-line no-unused-vars
+  const [targetWord, setTargetWord] = React.useState("SCOOT");
 
   return (
-    <div>
+    <form
+      onSubmit={(e) => {
+        // TODO: send data to server
+        e.preventDefault();
+        console.log(rules);
+      }}
+    >
       <label htmlFor="name">Game Name :</label>
       <input
         type="text"
         id="name"
-        onChange={() => dispatch(handleName)}
-        value={gameName}
+        onChange={(e) => {
+          setGameNameValue(e.target.value);
+          dispatch(handleName(e.target.value));
+        }}
+        value={gameNameValue}
       />
-      <lable htmlFor="nop">Number Of Players</lable>
+      <label htmlFor="nop">Number Of Players</label>
       <input
         type="number"
         id="nop"
-        onChange={() => dispatch(handleNbOfPlayers)}
-        value={nbOfPlayer}
+        onChange={(e) => {
+          setNbOfPlayers(e.target.value);
+          dispatch(handleNbOfPlayers(e.target.value));
+        }}
+        value={nbOfPlayers}
       />
-    </div>
+      <label htmlFor="hard">Hard Mode</label>
+      <input
+        type="checkbox"
+        id="hard"
+        onChange={() => {
+          setIsHard(!isHard);
+          dispatch(handleIsHard(!isHard));
+        }}
+        value={isHard}
+      />
+      <label htmlFor="redo">Allow Redo</label>
+      <input
+        type="checkbox"
+        id="redo"
+        onChange={() => {
+          setAllowRedo(!allowRedo);
+          dispatch(handleAllowRedo(!allowRedo));
+        }}
+        value={allowRedo}
+      />
+      <label htmlFor="try">Number Of Try</label>
+      <input
+        type="number"
+        id="try"
+        onChange={(e) => {
+          setNbOfTry(e.target.value);
+          dispatch(handleNbOfTry(e.target.value));
+        }}
+        value={nbOfTry}
+      />
+      <label htmlFor="target">Target Word</label>
+      <input
+        type="text"
+        id="target"
+        onChange={(e) => {
+          setTargetWord(e.target.value);
+          dispatch(handleTargetWord(e.target.value));
+          dispatch(handleLetters(e.target.value));
+        }}
+        value={targetWord}
+      />
+      <button type="submit">{"Next Step ->"}</button>
+    </form>
   );
 };
 
--- client/src/features/rules/rulesSlice.js
+++ client/src/features/rules/rulesSlice.js
@@ -3,6 +3,11 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   gameName: "",
   nbOfPlayer: 2,
+  isHard: false,
+  allowRedo: false,
+  NbOfTry: 1,
+  targetWord: "SCOOT",
+  letters: ["S", "C", "O", "O", "T"],
 };
 
 export const ruleSlice = createSlice({
@@ -15,12 +20,41 @@ export const ruleSlice = createSlice({
     handleNbOfPlayers: (state, action) => {
       state.nbOfPlayer = action.payload;
     },
+    handleIsHard: (state, action) => {
+      state.isHard = action.payload;
+    },
+    handleAllowRedo: (state, action) => {
+      state.allowRedo = action.payload;
+    },
+    handleNbOfTry: (state, action) => {
+      state.NbOfTry = action.payload;
+    },
+    handleTargetWord: (state, action) => {
+      state.targetWord = action.payload;
+    },
+    handleLetters: (state, action) => {
+      state.letters = Array.from(action.payload);
+    },
   },
 });
 
-export const { handleName, handleNbOfPlayers } = ruleSlice.actions;
+export const {
+  handleName,
+  handleNbOfPlayers,
+  handleIsHard,
+  handleAllowRedo,
+  handleNbOfTry,
+  handleTargetWord,
+  handleLetters,
+} = ruleSlice.actions;
 
+export const selectRules = (state) => state.rules;
 export const selectName = (state) => state.rules.gameName;
 export const selectNbOfPlayers = (state) => state.rules.nbOfPlayer;
+export const selectIsHard = (state) => state.rules.isHard;
+export const selectAllowRedo = (state) => state.rules.allowRedo;
+export const selectNbOfTry = (state) => state.rules.NbOfTry;
+export const selectTargetWord = (state) => state.rules.targetWord;
+export const selectLetters = (state) => state.rules.letters;
 
 export default ruleSlice.reducer;
