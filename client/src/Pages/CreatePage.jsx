c4f27f1eb0ccb0ac2f8e58528fc966dae74e70f0
Cyrheel_ <cyrilb38@hotmail.com>
2022-07-10 02:11:28 +0200

    init work on communication between client and api

--- client/src/Pages/CreatePage.jsx
+++ client/src/Pages/CreatePage.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import Rules from "../features/rules/Rules";
-import Player from "../features/players/Player";
 
 const CreatePage = () => {
   const navigate = useNavigate();
@@ -11,7 +10,6 @@ const CreatePage = () => {
       <h1>Create Game !</h1>
       <h2>Define your rules</h2>
       <Rules />
-      <Player />
     </div>
   );
 };
--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,13 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
 import rulesReducer from "../features/rules/rulesSlice";
-import { playerReducer, playerSReducer } from "../features/players/playerSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     rules: rulesReducer,
-    player: playerReducer,
-    playerS: playerSReducer,
   },
 });
