bc183c4a16eb8e3e7738ba734ad1125df947eb01
Cyrheel_ <cyrilb38@hotmail.com>
2023-02-04 14:00:10 +0100

    add unit test for manage status, and improve it

--- src/Algos/manageStatus.js
+++ src/Algos/manageStatus.js
@@ -1,16 +1,17 @@
 function manageStatus(players, game, definer) {
+  // Currently, set every player who copy to isActive false
+  // Also manage to set definer to isActive false cause he don't have to copy his own trick
+  let nextPlayers = players;
+  if (definer !== null) {
+    nextPlayers = players.map((p, i) => {
+      if (i === definer) {
+        return { ...p, isActive: false };
+      } else {
+        return p;
+      }
+    });
+  }
   if (game.currentAction === "copy") {
-    let nextPlayers;
-    if (definer) {
-      nextPlayers = players.map((p, i) => {
-        if (i === definer) {
-          return { ...p, isActive: false };
-        } else {
-          return p;
-        }
-      });
-    }
-
     nextPlayers = nextPlayers.map((p, i) => {
       if (i === game.currentPlayerId) {
         return { ...p, isActive: false };
@@ -18,8 +19,8 @@ function manageStatus(players, game, definer) {
         return p;
       }
     });
-    return nextPlayers;
   }
+  return nextPlayers;
 }
 
 export default manageStatus;
