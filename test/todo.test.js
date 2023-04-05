fb07c9bf3f3ce4c1ce9bfa024e6a91d8e88b2408
cyrheel <cyrilb38@hotmail.com>
2023-04-05 14:33:33 +0200

    remove old test

---
+++ test/todo.test.js
@@ -0,0 +1,8 @@
+import chai from "chai";
+const { expect: expector } = chai;
+
+describe("TODO: Write tests...", () => {
+  it("true to be true", () => {
+    expector(result).to.equal(null);
+  });
+});
