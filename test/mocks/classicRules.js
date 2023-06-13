7e85c1d78a13105d5f55761c985cbe050e34bae6
cyrheel <71268550+cyrheel@users.noreply.github.com>
2023-06-13 10:34:04 +0200

    (#33)
    * remove ut for now
    
    * remove ut from ci
    
    * clean code

--- test/mocks/classicRules.js
+++
@@ -1,7 +0,0 @@
-export const classicRules = {
-  gameName: "Game of Scoot",
-  isHard: false,
-  allowRedo: false,
-  nbOfTry: 2,
-  targetWord: "SCOOT",
-};
--- test/todo.test.js
+++
@@ -1,8 +0,0 @@
-import chai from "chai";
-const { expect: expector } = chai;
-
-describe("TODO: Write tests...", () => {
-  it("true to be true", () => {
-    expector(true).to.equal(true);
-  });
-});
